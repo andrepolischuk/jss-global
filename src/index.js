2eddd21ad0dfd9636bc79a193f491b5ffc905bf5
Oleg Slobodskoi <oleg008@gmail.com>
2016-12-21 01:38:10 +0100

    support nesting inside of global container, add tests

--- src/index.js
+++ src/index.js
@@ -1,27 +1,60 @@
+import {RulesContainer} from 'jss'
+
 const key = '@global'
 const prefixKey = '@global '
 
 class GlobalContainerRule {
+  type = 'global'
+
   constructor(name, styles, options) {
     this.name = name
     this.options = options
-    this.rules = []
+    this.rules = new RulesContainer({
+      ...options,
+      parent: this
+    })
 
     for (const selector in styles) {
-      const rule = options.jss.createRule(selector, styles[selector], {
-        ...options,
-        className: selector,
-        parent: this,
+      this.rules.add(selector, styles[selector], {
+        generateClassName: null,
         selector
       })
-      this.rules.push(rule)
     }
+
+    this.rules.process()
   }
 
-  toString(options) {
-    return this.rules
-      .map(rule => rule.toString(options))
-      .join('\n')
+  /**
+   * Get a rule.
+   */
+  getRule(name) {
+    return this.rules.get(name)
+  }
+
+  /**
+   * Create and register rule, run plugins.
+   */
+  addRule(name, style, options) {
+    const rule = this.rules.add(name, style, {
+      ...options,
+      generateClassName: null
+    })
+    this.options.jss.plugins.onProcessRule(rule)
+    return rule
+  }
+
+  /**
+   * Get index of a rule.
+   */
+  indexOf(rule) {
+    return this.rules.indexOf(rule)
+  }
+
+  /**
+   * Generates a CSS string.
+   */
+  toString() {
+    return this.rules.toString()
   }
 }
 
@@ -32,9 +65,9 @@ class GlobalPrefixedRule {
     const selector = name.substr(prefixKey.length)
     this.rule = options.jss.createRule(selector, style, {
       ...options,
-      className: selector,
       parent: this,
-      selector
+      selector,
+      generateClassName: null
     })
   }
 
@@ -50,11 +83,11 @@ function handleNestedGlobalContainerRule(rule) {
   if (!rules) return
 
   for (const name in rules) {
-    const className = `${rule.selector} ${name}`
+    const selector = `${rule.selector} ${name}`
     options.sheet.addRule(name, rules[name], {
       ...options,
-      className,
-      selector: className
+      selector,
+      generateClassName: null
     })
   }
 
@@ -70,8 +103,8 @@ function handlePrefixedGlobalRule(rule) {
     const scopedSelector = `${rule.selector} ${selector}`
     options.sheet.addRule(scopedSelector, style[prop], {
       ...options,
-      className: scopedSelector,
-      selector: scopedSelector
+      selector: scopedSelector,
+      generateClassName: null
     })
     delete style[prop]
   }
@@ -89,8 +122,24 @@ export default function jssGlobal() {
       return new GlobalContainerRule(name, styles, options)
     }
 
-    if (name.substr(0, prefixKey.length) === prefixKey) {
+    if (name[0] === '@' && name.substr(0, prefixKey.length) === prefixKey) {
       return new GlobalPrefixedRule(name, styles, options)
+    }
+
+    const {parent} = options
+
+    if (parent) {
+      if (
+        parent.type === 'global' ||
+        parent.options.parent.type === 'global'
+      ) {
+        options.global = true
+      }
+    }
+
+    if (options.global) {
+      options.selector = name
+      options.generateClassName = null
     }
 
     return null
